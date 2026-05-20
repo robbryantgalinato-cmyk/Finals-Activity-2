a7d4ead8148e18dc762c57874f50b8d247feb720
Rob Bryant T. Galinato <robbryantgalinato@gmail.com>
2026-05-20 23:30:05 +0800

    Add files via upload

---
+++ Activityno.1.php
@@ -0,0 +1,8 @@
+<?php
+echo "Name: Rob Bryant T. Galinato <br>";
+echo "Course: BSIT 1B <br>";
+
+echo "Ans: <br>";
+echo "1. The Symbol that used to end the statement is this (;) <br>";
+echo "2. In PHP, echo is slightly faster than print, but the difference is very small and usually not noticeable.<br>";
+?>
