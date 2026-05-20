0972f0e186fcc3e5578cafbf43e1b6d4bc2db7cc
Rob Bryant T. Galinato <robbryantgalinato@gmail.com>
2026-05-21 00:22:09 +0800

    Add files via upload

---
+++ Miniproject.php
@@ -0,0 +1,11 @@
+<?php
+$num1 = 20;
+$num2 = 10;
+
+echo "Sum: " . ($num1 + $num2) . "<br>";
+echo "Difference: " . ($num1 - $num2) . "<br>";
+echo "Product: " . ($num1 * $num2) . "<br>";
+echo "Quotient: " . ($num1 / $num2);
+?>
+
+
