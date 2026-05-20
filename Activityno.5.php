fd44df00a65a92c81e82f99f633f4f21a4f37626
Rob Bryant T. Galinato <robbryantgalinato@gmail.com>
2026-05-21 00:05:30 +0800

    Add files via upload

---
+++ Activityno.5.php
@@ -0,0 +1,9 @@
+<?php
+$a = 15;
+$b = 5;
+
+echo "Addition: " . ($a + $b) . "<br>";
+echo "Subtraction: " . ($a - $b) . "<br>";
+echo "Multiplication: " . ($a * $b) . "<br>";
+echo "Division: " . ($a / $b);
+?>
